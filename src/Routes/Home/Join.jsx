6be04797be0c1fc6b4b90d5f05947d4e346e295d
Kieran Noble <kieran@aylo.net>
2022-11-03 09:18:13 +0000

    FIx Link

--- src/Routes/Home/Join.jsx
+++ src/Routes/Home/Join.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function Join() {
 
   return (
@@ -7,7 +9,7 @@ function Join() {
         <p className="text-sm dark:text-gray-200">Sign up today and receive a discount voucher to use in the shop!</p>
       </div>
       <div className="flex justify-center">
-        <button className="bg-jack hover:bg-jack-light text-white font-bold p-2 px-6 rounded-full text-center hover:underline">Sign Up</button>
+        <Link to="/auth/register" className="bg-jack hover:bg-jack-light text-white font-bold p-2 px-6 rounded-full text-center hover:underline">Sign Up</Link>
       </div>
     </div>
   )
