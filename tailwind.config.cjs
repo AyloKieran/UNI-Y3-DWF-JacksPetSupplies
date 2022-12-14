824d5886591bf5f773b94b98a179d2166efe7c75
AyloKieran <kieran@aylo.net>
2022-12-14 09:45:13 +0000

    Add screen themes breakpoints & Update packages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,9 +6,16 @@ module.exports = {
       colors: {
         jack: {
           DEFAULT: '#406be2',
-          light: '#5f83e7'
+          light: '#5f83e7',
         }
       }
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
     },
   },
   plugins: [
